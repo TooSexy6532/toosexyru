157c83fb63af8aefe8e51c8a8a2bec8039fa3452
TooSexy6532 <papkovdmitry@gmail.com>
2021-01-08 17:01:43 +0500

    final

---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.querySelector('.en_switcher').onclick = showContent
+document.querySelector('.ru_switcher').onclick = showContent
+const lngButtonEn = document.querySelector('.en_switcher')
+const lngButtonRu = document.querySelector('.ru_switcher')
+console.log(lngButtonEn)
+console.log(lngButtonRu)
+function showContent () {
+  if (this.classList.contains('en')) {
+  let x = document.querySelectorAll(".en")
+  for (let i = 0; i < x.length; i++) {
+  x[i].style.display = 'none'
+  }
+  let y = document.querySelectorAll(".ru")
+  for (let i = 0; i < y.length; i++) {
+  y[i].style.display = 'block'
+  }
+  console.log('Переключили на русский')
+  } else {
+  let z = document.querySelectorAll(".ru")
+  for (let i = 0; i < z.length; i++) {
+  z[i].style.display = 'none'
+  }
+  let c = document.querySelectorAll(".en")
+  for (let i = 0; i < c.length; i++) {
+  c[i].style.display = 'block'
+  }
+  console.log('Переключили на английский')
+  }
+}
+function openForm() {
+    document.querySelector('.modal_dialog').style.display = "block"
+    console.log('Открыл форму')
+}
+function closeForm() {
+    document.querySelector('.modal_dialog').style.display = "none";
+    console.log('Закрыл форму')
+}
