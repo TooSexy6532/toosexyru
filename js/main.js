fcc7b7fab69b95d7b2eaa3f791922f343c69bad8
TooSexy6532 <papkovdmitry@gmail.com>
2021-01-08 22:21:16 +0500

    final 0.1

--- js/main.js
+++ js/main.js
@@ -1,33 +1,36 @@
 document.querySelector('.en_switcher').onclick = showContent
 document.querySelector('.ru_switcher').onclick = showContent
-function showContent () {
+
+function showContent() {
   if (this.classList.contains('en')) {
-  let x = document.querySelectorAll(".en")
-  for (let i = 0; i < x.length; i++) {
-  x[i].style.display = 'none'
-  }
-  let y = document.querySelectorAll(".ru")
-  for (let i = 0; i < y.length; i++) {
-  y[i].style.display = 'block'
-  }
-  console.log('Переключили на русский')
+    let x = document.querySelectorAll(".en")
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.display = 'none'
+    }
+    let y = document.querySelectorAll(".ru")
+    for (let i = 0; i < y.length; i++) {
+      y[i].style.display = 'block'
+    }
+    console.log('Переключили на русский')
   } else {
-  let z = document.querySelectorAll(".ru")
-  for (let i = 0; i < z.length; i++) {
-  z[i].style.display = 'none'
-  }
-  let c = document.querySelectorAll(".en")
-  for (let i = 0; i < c.length; i++) {
-  c[i].style.display = 'block'
-  }
-  console.log('Переключили на английский')
+    let z = document.querySelectorAll(".ru")
+    for (let i = 0; i < z.length; i++) {
+      z[i].style.display = 'none'
+    }
+    let c = document.querySelectorAll(".en")
+    for (let i = 0; i < c.length; i++) {
+      c[i].style.display = 'block'
+    }
+    console.log('Переключили на английский')
   }
 }
+
 function openForm() {
-    document.querySelector('.modal_dialog').style.display = "block"
-    console.log('Открыл форму')
+  document.querySelector('.modal_dialog').style.display = "block"
+  console.log('Открыл форму')
 }
+
 function closeForm() {
-    document.querySelector('.modal_dialog').style.display = "none";
-    console.log('Закрыл форму')
+  document.querySelector('.modal_dialog').style.display = "none";
+  console.log('Закрыл форму')
 }
