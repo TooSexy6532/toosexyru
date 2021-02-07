f64d66c51c4f2ffaa4592f8dcdb855ead244dd7f
TooSexy6532 <papkovdmitry@gmail.com>
2021-02-07 09:37:29 +0500

    1

--- js/main.js
+++ js/main.js
@@ -1,46 +1,126 @@
-document.querySelector('.en_switcher').onclick = showContent
-document.querySelector('.ru_switcher').onclick = showContent
-const popup = document.querySelector('.modal_dialog')
-const popupForm = document.querySelector('.modal_form')
-console.log(popup)
-console.log(popupForm)
-function showContent() {
-  if (this.classList.contains('en')) {
-    let x = document.querySelectorAll(".en")
-    for (let i = 0; i < x.length; i++) {
-      x[i].style.display = 'none'
-    }
-    let y = document.querySelectorAll(".ru")
-    for (let i = 0; i < y.length; i++) {
-      y[i].style.display = 'block'
-    }
-    console.log('Переключили на русский')
-  } else {
-    let z = document.querySelectorAll(".ru")
-    for (let i = 0; i < z.length; i++) {
-      z[i].style.display = 'none'
-    }
-    let c = document.querySelectorAll(".en")
-    for (let i = 0; i < c.length; i++) {
-      c[i].style.display = 'block'
-    }
-    console.log('Переключили на английский')
+let anchors = document.querySelectorAll('.header__navbar a'),
+    body = document.querySelector("body"),
+    bHeight = body.getBoundingClientRect().height,
+    screenY = window.innerHeight / 8,
+    nav = document.querySelector(".header");
+
+const sections = document.querySelectorAll('.section'),
+      lngButton = document.querySelectorAll('.lng_switcher_button'),
+      eng = document.querySelectorAll('.en'),
+      ru = document.querySelectorAll('.ru'),
+      lngSwitcher = document.querySelector('.lng_switcher'),
+      popup = document.querySelector('.modal-warp'),
+      popupForm = document.querySelector('.modal-warp__form'),
+      formBtn = document.querySelectorAll('.btn');
+
+  function switchToEng() {
+    ru.forEach(item => {
+      item.classList.add('hidden');
+      item.classList.remove('visible', 'fade');
+    });
+
+    eng.forEach(item => {
+      item.classList.add('visible', 'fade');
+      item.classList.remove('hidden');
+    });
+
+    lngButton.forEach(item => {
+      item.classList.remove('lng_switcher_button_active');
+    });
   }
-}
 
-function openForm() {
-  popup.style.display = "block"
-  popup.classList.add("visible")
-  console.log('Открыл форму')
-}
+  function switchToRu() {
+    eng.forEach(item => {
+      item.classList.add('hidden', 'fade');
+      item.classList.remove('visible', 'fade');
+    });
 
-function closeForm() {
-  popup.style.display = "none"
-  popup.classList.remove("visible")
-  console.log('Закрыл форму')
-}
+    ru.forEach(item => {
+      item.classList.add('visible', 'fade');
+      item.classList.remove('hidden', 'fade');
+    });
 
-//
-// document.onclick = function() {
-//     popup.classList.contains("visible") ? console.log('Форма открыта, кликнул вне её') : console.log('Форма закрыта');
-// }
+    lngButton.forEach(item => {
+      item.classList.remove('lng_switcher_button_active');
+    });
+  }
+
+  lngSwitcher.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target && target.classList.contains('rus')) {
+      switchToRu();
+      target.classList.add('lng_switcher_button_active', 'fade');
+    } else if (target && target.classList.contains('eng')) {
+      switchToEng();
+      target.classList.add('lng_switcher_button_active', 'fade');
+    }
+  });
+
+  function openForm() {
+    popup.classList.add("visible", 'fade');
+    popup.classList.remove("hidden");
+  }
+
+  function closeForm() {
+    popup.classList.remove("visible", 'fade');
+    popup.classList.add("hidden");
+
+  }
+
+  formBtn.forEach(item => {
+    item.addEventListener('click', (event) => {
+      const target = event.target;
+      if (target && event.currentTarget.classList.contains('footer__button')) {
+        openForm();
+      } else if (target && event.currentTarget.classList.contains('cansel')) {
+        closeForm();
+      }
+    });
+  });
+
+  popup.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target && target.classList.contains('modal-warp')) {
+      closeForm();
+    }
+  });
+
+
+  window.onscroll = () => {
+    if (pageYOffset > screenY) {
+      nav.classList.add("header-fixed");
+      nav.classList.remove("header__navbar");
+    } else {
+      nav.classList.remove("header-fixed");
+    }
+  };
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute('href').substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  }
+
+  window.addEventListener('scroll', () => {
+    let scrollDistance = window.scrollY;
+    
+    if (window.innerWidth > 1024) {
+      sections.forEach((el, i) => {
+        if (el.offsetTop - document.querySelector('.header__navbar').clientHeight <= scrollDistance) {
+          document.querySelectorAll('.side-navbar__dot').forEach((el) => {
+            if (el.classList.contains('side-navbar__dot-active')) {
+              el.classList.remove('side-navbar__dot-active');
+            }
+          });
+          document.querySelectorAll('.side-navbar__dot')[i].classList.add('side-navbar__dot-active');
+        }
+      });
+    }
+  });
